2d690588b00a728d4eebebcbc151c7896d60661e
rodrib <rodribogado50@gmail.com>
2024-04-12 16:24:44 -0300

    update cluster

--- pbv2/pages/02_cluster.py
+++ pbv2/pages/02_cluster.py
@@ -2,7 +2,9 @@ import streamlit as st
 import requests
 
 # Define la URL del servidor FastAPI
-API_URL = "https://backpv2.onrender.com/api/prediccion_cluster"
+API_URL = "https://backpv2-18.onrender.com/api/prediccion_cluster"
+
+
 
 # Título y descripción de la aplicación Streamlit
 st.title("Predicción de Cluster")

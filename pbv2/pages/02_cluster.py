0c8e0e6a1f6efc9d7d205a55b68f215c25c17b67
rodrib <rodribogado50@gmail.com>
2024-04-12 17:25:33 -0300

    update cluster

--- pbv2/pages/02_cluster.py
+++ pbv2/pages/02_cluster.py
@@ -12,8 +12,9 @@ st.title("Cálculo de Correlación")
 st.write("Ingrese los datos para calcular la correlación entre EDAD y DX1:")
 
 # Recopila los datos de entrada del usuario
-edad = st.number_input("Ingrese la edad:")
-dx1 = st.number_input("Ingrese el valor DX1:")
+edad = st.number_input("Ingrese la edad:", step=1, value=0)
+dx1 = st.number_input("Ingrese el valor DX1:", step=1, value=0)
+
 
 # Botón para calcular la correlación
 if st.button("Calcular Correlación"):

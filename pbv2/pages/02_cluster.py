78559498ef5316a216d8862867cd9a5c2eb6beec
rodrib <rodribogado50@gmail.com>
2024-04-12 16:41:54 -0300

    update cluster

--- pbv2/pages/02_cluster.py
+++ pbv2/pages/02_cluster.py
@@ -8,27 +8,29 @@ API_URL = "https://backpv2-18.onrender.com/api/prediccion_cluster"
 st.title("Predicción de Cluster")
 st.write("Ingrese los datos para predecir el cluster:")
 
-# Recopila los datos de entrada del usuario
+# Campos de entrada para que el usuario ingrese los datos
 RECIDIVA = st.slider("RECIDIVA", min_value=0, max_value=1, step=1)
 DX1 = st.slider("DX1", min_value=0, max_value=1, step=1)
 EDAD = st.slider("EDAD", min_value=0, max_value=100, step=1)
 GRADO1 = st.slider("GRADO1", min_value=1, max_value=3, step=1)
 HER21 = st.slider("HER21", min_value=0, max_value=1, step=1)
 
-# Realiza una solicitud HTTP POST al servidor FastAPI con los datos como cuerpo de la solicitud
-data = {
-    "RECIDIVA": RECIDIVA,
-    "DX1": DX1,
-    "EDAD": EDAD,
-    "GRADO1": GRADO1,
-    "HER21": HER21
-}
-response = requests.post(API_URL, json=data)
-
-
-# Verifica si la solicitud fue exitosa y muestra los resultados
-if response.status_code == 200:
-    result = response.json()
-    st.write("Cluster predicho:", result["cluster_predicho"])
-else:
-    st.error("Error al realizar la predicción. Inténtalo de nuevo más tarde.")
+# Botón para hacer la predicción
+if st.button("Hacer Predicción"):
+    # Realizar solicitud HTTP GET a la API con los datos ingresados por el usuario
+    url = "https://backpv2-18.onrender.com/api/prediccion_cluster"
+    parametros = {
+        "RECIDIVA": RECIDIVA,
+        "DX1": DX1,
+        "EDAD": EDAD,
+        "GRADO1": GRADO1,
+        "HER21": HER21
+    }
+    response = requests.get(url, params=parametros)
+    
+    # Verificar si la solicitud fue exitosa
+    if response.status_code == 200:
+        resultado_prediccion = response.json()
+        st.write("Cluster predicho:", resultado_prediccion["cluster_predicho"])
+    else:
+        st.error("Error al hacer la predicción")

7e56545f2e52427eebea67a5bde1252021c774ee
rodrib <rodribogado50@gmail.com>
2024-04-12 17:17:19 -0300

    update cluster

--- pbv2/pages/02_cluster.py
+++ pbv2/pages/02_cluster.py
@@ -1,36 +1,32 @@
 import streamlit as st
 import requests
 
-# Define la URL del servidor FastAPI
-API_URL = "https://backpv2-18.onrender.com/api/prediccion_cluster"
+import streamlit as st
+import requests
+
+import streamlit as st
+import requests
 
 # Título y descripción de la aplicación Streamlit
-st.title("Predicción de Cluster")
-st.write("Ingrese los datos para predecir el cluster:")
+st.title("Cálculo de Correlación")
+st.write("Ingrese los datos para calcular la correlación entre EDAD y DX1:")
 
-# Campos de entrada para que el usuario ingrese los datos
-RECIDIVA = st.slider("RECIDIVA", min_value=0, max_value=1, step=1)
-DX1 = st.slider("DX1", min_value=0, max_value=1, step=1)
-EDAD = st.slider("EDAD", min_value=0, max_value=100, step=1)
-GRADO1 = st.slider("GRADO1", min_value=1, max_value=3, step=1)
-HER21 = st.slider("HER21", min_value=0, max_value=1, step=1)
+# Recopila los datos de entrada del usuario
+edad = st.number_input("Ingrese la edad:")
+dx1 = st.number_input("Ingrese el valor DX1:")
 
-# Botón para hacer la predicción
-if st.button("Hacer Predicción"):
-    # Realizar solicitud HTTP GET a la API con los datos ingresados por el usuario
-    url = "https://backpv2-18.onrender.com/api/prediccion_cluster"
-    parametros = {
-        "RECIDIVA": RECIDIVA,
-        "DX1": DX1,
-        "EDAD": EDAD,
-        "GRADO1": GRADO1,
-        "HER21": HER21
-    }
-    response = requests.get(url, params=parametros)
+# Botón para calcular la correlación
+if st.button("Calcular Correlación"):
+    # Realizar solicitud HTTP GET al servidor FastAPI con los datos ingresados por el usuario
+    url = "https://backpv2-18.onrender.com/api/correlacion"
+    params = {"EDAD": edad, "DX1": dx1}
+    response = requests.get(url, params=params)
     
     # Verificar si la solicitud fue exitosa
     if response.status_code == 200:
-        resultado_prediccion = response.json()
-        st.write("Cluster predicho:", resultado_prediccion["cluster_predicho"])
+        resultado_correlacion = response.json()
+        st.write("Correlación calculada:", resultado_correlacion["correlacion"])
     else:
-        st.error("Error al hacer la predicción")
+        st.error("Error al calcular la correlación. Inténtalo de nuevo más tarde.")
+
+

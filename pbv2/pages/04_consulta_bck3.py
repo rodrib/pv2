5a027148465527d3de6722d475649617cd487496
rodrib <rodribogado50@gmail.com>
2024-04-16 22:46:25 -0300

    create consulta_bd3

---
+++ pbv2/pages/04_consulta_bck3.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import requests
+
+st.title("Búsqueda en ClinVar")
+
+# Solicitar al usuario que ingrese el nombre del gen y la región variante
+gene_name = st.text_input("Ingrese el nombre del gen (gene_name):")
+variant_region = st.text_input("Ingrese la región variante (variant_region):")
+
+# Verificar si se ingresaron valores y realizar la búsqueda si es así
+if gene_name and variant_region:
+    st.write(f"Realizando búsqueda en ClinVar para {gene_name} - {variant_region}...")
+    response = requests.get(f"http://localhost:8000/api/clinvar_search?gene_name={gene_name}&variant_region={variant_region}")
+    if response.status_code == 200:
+        resultados = response.json()
+        st.write("Resultados:")
+        st.write(resultados)
+    else:
+        st.write("Error al realizar la búsqueda.")
